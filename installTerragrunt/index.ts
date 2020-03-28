8cb229cf9f6ad17a648e93c1ab39f92a364e4658
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2020-03-28 04:42:20 -0700

    format code

--- installTerragrunt/index.ts
+++ installTerragrunt/index.ts
@@ -1,44 +1,58 @@
-import taskLib = require('azure-pipelines-task-lib/task');
-import toolLib = require('azure-pipelines-tool-lib/tool');
-import os = require('os');
+import taskLib = require("azure-pipelines-task-lib/task");
+import toolLib = require("azure-pipelines-tool-lib/tool");
+import os = require("os");
 
 async function run() {
-    try {
-        const versionNumber: string = taskLib.getInput('terragruntversion', true);
+  try {
+    const versionNumber: string = taskLib.getInput("terragruntversion", true);
 
-        if ((versionNumber == '') || (versionNumber == null)) {
-            taskLib.setResult(taskLib.TaskResult.Failed, 'Invalid version number given');
-            return;
-        }
-
-        let downloadUrl = downloadLink(versionNumber, os.platform(), os.arch());
-
-        const downloaded: string = await toolLib.downloadTool(downloadUrl);
-
-        const cached: string = await toolLib.cacheFile(downloaded,`terragrunt.exe`,`terragrunt`, versionNumber);
-
-        toolLib.prependPath(cached);
-
-        taskLib.setResult(taskLib.TaskResult.Succeeded, 'Terragrunt has been installed.');
+    if (versionNumber == "" || versionNumber == null) {
+      taskLib.setResult(
+        taskLib.TaskResult.Failed,
+        "Invalid version number given"
+      );
+      return;
     }
-    catch (err) {
-        taskLib.setResult(taskLib.TaskResult.Failed, err.message);
-    }
+
+    let downloadUrl = downloadLink(versionNumber, os.platform(), os.arch());
+
+    const downloaded: string = await toolLib.downloadTool(downloadUrl);
+
+    const cached: string = await toolLib.cacheFile(
+      downloaded,
+      `terragrunt.exe`,
+      `terragrunt`,
+      versionNumber
+    );
+
+    toolLib.prependPath(cached);
+
+    taskLib.setResult(
+      taskLib.TaskResult.Succeeded,
+      "Terragrunt has been installed."
+    );
+  } catch (err) {
+    taskLib.setResult(taskLib.TaskResult.Failed, err.message);
+  }
 }
 
-const downloadLink = function(version: string, os: string, arch: string): string {
-    if (os == 'win32') {
-        os = 'windows';
-    }
+const downloadLink = function(
+  version: string,
+  os: string,
+  arch: string
+): string {
+  if (os == "win32") {
+    os = "windows";
+  }
 
-    if (arch == 'x32') {
-        arch = '386';
-    } else if (arch == 'x64') {
-        arch = 'amd64';
-    }
-    
-    // Add linux and MacOS to this.
-    return `https://github.com/gruntwork-io/terragrunt/releases/download/v${version}/terragrunt_${os}_${arch}.exe`;
-}
+  if (arch == "x32") {
+    arch = "386";
+  } else if (arch == "x64") {
+    arch = "amd64";
+  }
+
+  // Add linux and MacOS to this.
+  return `https://github.com/gruntwork-io/terragrunt/releases/download/v${version}/terragrunt_${os}_${arch}.exe`;
+};
 
 run();
